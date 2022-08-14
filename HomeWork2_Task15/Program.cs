92842b4aa2191a08acf1da67481dff316be99111
Aleksandr <belyakov71@yandex.ru>
2022-08-15 01:13:23 +0300

    Домашняя работа. Задача №15

---
+++ HomeWork2_Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа принимает на вход цифру 
+// обозначающую день недели
+// проверяет является ли этот день выходным
+
+Console.WriteLine("Введите число обозначающее день недели: ");
+int DayNum = Convert.ToInt32(Console.ReadLine());
+
+if (DayNum == 6 || DayNum == 7)
+{
+    Console.WriteLine("Этот день - выходной");
+}
+else
+{
+    Console.WriteLine("Этот день - не выходной");
+} 
