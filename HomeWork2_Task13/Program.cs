3ba31e05fee9db1435d318cb7a8e7664e3adc574
Aleksandr <belyakov71@yandex.ru>
2022-08-15 00:33:50 +0300

    Домашняя работа. Задача №13

---
+++ HomeWork2_Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа выводит третью цифру заданного числа
+// или сообщает что третьей цифры нет.
+
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+int num1 = number / 100;
+int num2 = num1 % 10;
+
+if (num2 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра: {num2}");
+} 
