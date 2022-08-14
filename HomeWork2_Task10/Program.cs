45694553010cc099daf76622661bb88616f09b36
Aleksandr <belyakov71@yandex.ru>
2022-08-14 23:05:23 +0300

    Домашняя работа. Задача №10

--- HomeWork2_Task10/Program.cs
+++ HomeWork2_Task10/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа принимает трёхзначное число
+// на выходе показывает вторую цифру.
+
+Console.WriteLine("Введите трёхзначное, целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num1 = number / 10;
+int num2 = num1 % 10;
+
+Console.WriteLine($"Вторая цифра: {num2}");
